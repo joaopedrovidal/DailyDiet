6b3a131bcd02ea8d6dd7c42d373b529646dc6862
João Pedro Vidal <ppvidalsantos@gmail.com>
2026-07-09 14:05:47 -0300

    chore: implementa variável de ambiente

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,15 @@
+import 'dotenv/config'
 import knex from 'knex';
 import type { Knex } from 'knex'
+
+if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL not found!')
+}
 
 export const config: Knex.Config = {
     client: 'sqlite',
     connection: {
-        filename: './db/app.db',
+        filename: process.env.DATABASE_URL,
     },
     useNullAsDefault: true,
     migrations: {
